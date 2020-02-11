ba23957e68c4584dec8f7d8b14924c5270621db5
Baba Fakruddin <iBabafakruddin@gmail.com>
2020-02-11 19:48:19 +0530

    4. Working with state

--- App.js
+++ App.js
@@ -1,13 +1,41 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Button } from "react-native";
 
 import HomeScreen from "./src/screens/HomeScreen";
 
 class ClassComp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.cnt = 0;
+    this.state = { count: 0 };
+
+    // This binding is necessary to make `this` work in the callback
+    this.incrementCnt = this.incrementCnt.bind(this);
+    this.incrementState = this.incrementState.bind(this);
+  }
+
+  incrementCnt() {
+    this.cnt = this.cnt + 1;
+    console.log(this.cnt);
+  }
+
+  incrementState() {
+    this.setState({ count: this.state.count + 1 });
+    console.log(this.state.count);
+  }
+
   render() {
     return (
       <View>
         <Text>Class Component, Name is {this.props.name}</Text>
+        <Button
+          title={`Count with cnt: ${this.cnt} +`}
+          onPress={this.incrementCnt}
+        />
+        <Button
+          title={`Count with State: ${this.state.count} +`}
+          onPress={this.incrementState}
+        />
       </View>
     );
   }
@@ -16,8 +44,8 @@ class ClassComp extends React.Component {
 const App = () => {
   return (
     <View>
-      <ClassComp name="Admin" />
       <HomeScreen name="Admin" />
+      <ClassComp name="Admin" />
     </View>
   );
 };
