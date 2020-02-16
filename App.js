d5661edb121796cff2e9690862e074ab76fcc36b
Baba Fakruddin <iBabafakruddin@gmail.com>
2020-02-16 10:12:41 +0530

    Create empty screens

--- App.js
+++ App.js
@@ -2,22 +2,15 @@ import React from "react";
 import { View, Text } from "react-native";
 
 import HomeScreen from "./src/screens/HomeScreen";
-
-class ClassComp extends React.Component {
-  render() {
-    return (
-      <View>
-        <Text>Class Component</Text>
-      </View>
-    );
-  }
-}
+import ShelfScreen from "./src/screens/ShelfScreen";
+import ReaderScreen from "./src/screens/ReaderScreen";
 
 const App = () => {
   return (
     <View>
-      <ClassComp />
       <HomeScreen />
+      <ShelfScreen />
+      <ReaderScreen />
     </View>
   );
 };
---
+++ src/screens/ShelfScreen.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const ShelfScreen = () => {
+  return (
+    <View>
+      <Text>ShelfScreen Component</Text>
+    </View>
+  );
+};
+
+export default ShelfScreen;
+
