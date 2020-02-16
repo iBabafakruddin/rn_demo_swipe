d7f548c40b059db3d63d171136b6fc7e4022eb77
Baba Fakruddin <iBabafakruddin@gmail.com>
2020-02-16 10:21:21 +0530

    Added layout/navigator

--- App.js
+++ App.js
@@ -1,17 +1,16 @@
 import React from "react";
 import { View, Text } from "react-native";
+import { createAppContainer, createSwitchNavigator } from "react-navigation";
+import { createBottomTabNavigator } from "react-navigation-tabs";
 
 import HomeScreen from "./src/screens/HomeScreen";
 import ShelfScreen from "./src/screens/ShelfScreen";
 import ReaderScreen from "./src/screens/ReaderScreen";
 
-const App = () => {
-  return (
-    <View>
-      <HomeScreen />
-      <ShelfScreen />
-      <ReaderScreen />
-    </View>
-  );
-};
-export default App;
+const bottomTabBar = createBottomTabNavigator({
+  Home: { screen: HomeScreen },
+  Shelf: { screen: ShelfScreen },
+  Read: { screen: ReaderScreen }
+});
+
+export default createAppContainer(bottomTabBar);
