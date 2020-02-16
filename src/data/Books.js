12aded19fcec62a800416d213826636afa6a81e9
Baba Fakruddin <iBabafakruddin@gmail.com>
2020-02-16 11:49:54 +0530

    Added Contents Shelf Screen

---
+++ src/data/Books.js
@@ -0,0 +1,45 @@
+export default {
+  Books: [
+    {
+      id: 1,
+      title: "Wings of Fire: An Autobiography of Abdul Kalam",
+      author: " Arun Tiwar, A. P. J. Abdul Kalam ",
+      image: "wings_of_fire.jpg",
+      description:
+        "Every common man who by his sheer grit and hard work achieves succes\
+             should share his story with the rest for they may find inspiration and strength to go on, in his story.\
+            The 'Wings of Fire' is one such autobiography by visionary scientist Dr. APJ Abdul Kalam, \
+            who from very humble beginnings rose to be the President of India. \
+            The book is full of insights, personal moments and life experiences of Dr. Kalam. \
+            It gives us an understanding on his journey of success."
+    },
+    {
+      id: 2,
+      title: "The 7 Habits of Highly Effective People ",
+      author: "R. Stephen Covey",
+      image: "7habits.jpg",
+      description:
+        "It is rightly said that habits make or break a man. \
+      If you want to know why you are not doing something right, \
+      sometimes all you need is to perform an analysis of your habits and consider altering them. \
+      Because sometimes it’s not about what you do, but more about how you do it! And that’s where your habits play a very important role."
+    },
+    {
+      id: 3,
+      title: "The Alchemist",
+      author: "Paulo Coelho",
+      image: "alchemist.jpg"
+    },
+    {
+      id: 4,
+      title: "Sita: Warrior of Mithila ",
+      author: "Amish Tripathi",
+      image: "sita.jpg",
+      description:
+        "Based on the Ramayana, the story of Lady Sita, written by the multi-million bestselling Indian Author Amish; \
+        the author who has transformed Indian Fiction with his unique combination of mystery, mythology, religious symbolism and philosophy. \
+        In this book, you will follow Lady Sita's journey from an Adopted Child to the Prime Minister to finding her true calling.\
+         You will find all the familiar characters you have heard of, like Lord Ram, Lakshman etc"
+    }
+  ]
+};
--- src/screens/ShelfScreen.js
+++ src/screens/ShelfScreen.js
@@ -1,13 +1,28 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Image, ScrollView } from "react-native";
+import Data from "../data/Books";
 
 const ShelfScreen = () => {
+  const { Books } = Data;
   return (
-    <View>
+    <ScrollView>
       <Text>ShelfScreen Component</Text>
-    </View>
+      {Books.map(Book => {
+        return (
+          <View key={Book.id} style={{ flex: 1, flexDirection: "row" }}>
+            <Image
+              source={require(`../../assets/${Book.image}`)}
+              style={{ height: 150, width: 80, alignSelf: "flex-start" }}
+            />
+            <View style={{ alignSelf: "flex-end", marginTop: 0 }}>
+              <Text>{Book.title}</Text>
+              <Text>{Book.author}</Text>
+            </View>
+          </View>
+        );
+      })}
+    </ScrollView>
   );
 };
 
 export default ShelfScreen;
-
